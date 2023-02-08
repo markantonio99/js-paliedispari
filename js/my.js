4cb2486b93d54024c4ccebae4f7d9dbf01d252bc
“markantonio99” <markomakkia@gmail.com>
2023-02-08 15:13:40 +0100

    prova 1

---
+++ js/my.js
@@ -0,0 +1,42 @@
+let parolaInserita = prompt('inserisci la parola per controllare se è palindroma')
+console.log(parolaInserita)
+
+const parolaArray = []
+console.log(parolaArray)
+
+parolaArray.push(parolaInserita)
+console.log(parolaArray)
+
+
+
+// let keyDiv = parseInt(parolaInserita)
+// console.log(keyDiv)
+
+// let numKey = (keyDiv / contkey)
+// console.log(numKey)
+
+
+
+function controlkey (key) {
+    
+    let multiKey 
+
+    for ( let i = 0; i < key.lenght; i++){
+        multiKey =[i]
+    }
+
+   
+
+    // for ( let i = 0; i < key.lenght; i++ ) {
+
+    //     console.log(i)
+    // }
+
+
+
+}
+
+const numkey = controlkey(parolaArray)
+console.log(numkey)
+ 
+ 
