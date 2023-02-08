ae62feaeb19f71d46be6dca11410c825f6f7232d
“markantonio99” <markomakkia@gmail.com>
2023-02-08 16:45:33 +0100

    prova dadi 2

--- dadi /js/dadi.js
+++ dadi /js/dadi.js
@@ -5,18 +5,81 @@
 
  buttonElement.addEventListener('click', function()) {
     console.log('lancia dadi')
+
+
+   let numEle = document.getElementById('numeroutente')
+
+   let numInserito = parseInt(numEle.value) 
+   console.log(numInserito)
+
+   let sceltanumeri = document.getElementById('select')
+   console.log(sceltanumeri)
+
+   let modSceltaNum = sceltanumeri.value
+   console.log(modSceltaNum)
+
+
+  let numeroTotale = numbergenerate + numInserito
+  console.log(numeroTotale)
+
+
+  let numPari = numeroTotale % 2 === 0
+
+  let risultatoFinale = ""
+
+     if ( numeroTotale === 'pari' ){
+       
+          risultatoFinale.innerHTML = 'Hai' + ' Vinto ' 
+         }
+
+    else if (numeroTotale != numPari){
+        
+        risultatoFinale.innerHTML = 'Hai' + ' Perso ' 
+
+         }
+
+    else {
+
+        risultatoFinale.innerHTML = 'Hai' + ' Pareggiato ' 
+    }
+
+
+
+
  }
-   
-
-
-//  buttonElement.addEventListener('click', function()){
-//     console.log(lancia i dadi)
-//  }
+    function numbergenerate(val){
+    const numeroPc = Math.floor(Math.random() * (6 - 1) + 1);
+ }
 
 
 
 
 
-//  function numbergenerate(val){
-//     const numeroPc = Math.floor(Math.random() * (7 - 1) + 1);
-//  }
+
+
+
+
+
+
+
+
+
+// agganciare bottone ad addEventListener
+//
+// creare casella di scelta se pari o dispari
+//
+// creare casella di inserimento numero 
+// creare una variabile con value pere registrare il numero inserito
+// // farsi dare un numero dal cliente
+
+// 
+// 
+// creare la funzione di numeri random
+// 
+// 
+// 
+// 
+// 
+
+
+
