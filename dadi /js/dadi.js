fbbc61f2d3afd704d8c3eccb27a11b6e8dc3c480
“markantonio99” <markomakkia@gmail.com>
2023-02-08 15:57:50 +0100

    prova 1 cartella dadi

---
+++ dadi /js/dadi.js
@@ -0,0 +1,22 @@
+ 
+ const buttonElement = document.getElementById('button')
+ console.log(buttonElement)
+
+
+ buttonElement.addEventListener('click', function()) {
+    console.log('lancia dadi')
+ }
+   
+
+
+//  buttonElement.addEventListener('click', function()){
+//     console.log(lancia i dadi)
+//  }
+
+
+
+
+
+//  function numbergenerate(val){
+//     const numeroPc = Math.floor(Math.random() * (7 - 1) + 1);
+//  }
