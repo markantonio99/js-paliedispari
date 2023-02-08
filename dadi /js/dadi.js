318fcff6ada72c1c45224a5ca557a1a5536bf5c3
“markantonio99” <markomakkia@gmail.com>
2023-02-08 17:02:19 +0100

    prova 3

--- dadi /js/dadi.js
+++ dadi /js/dadi.js
@@ -2,41 +2,58 @@
  const buttonElement = document.getElementById('button')
  console.log(buttonElement)
 
-
+// aggancio ddl tasto
  buttonElement.addEventListener('click', function()) {
     console.log('lancia dadi')
 
-
+// aggancio ddl numero inserito
    let numEle = document.getElementById('numeroutente')
 
+// cambio da stringa a numero
    let numInserito = parseInt(numEle.value) 
    console.log(numInserito)
-
+   
+// aggancio della select per la scelta gioco 
    let sceltanumeri = document.getElementById('select')
    console.log(sceltanumeri)
 
+ //creazione variabile dell modalità di gioco  
    let modSceltaNum = sceltanumeri.value
    console.log(modSceltaNum)
 
-
+ //creazione variabile della  somma numero utente + numero pc
   let numeroTotale = numbergenerate + numInserito
   console.log(numeroTotale)
 
-
+ //creazione variabile per stabilire numero pari
   let numPari = numeroTotale % 2 === 0
 
+//creazione variabile vuota per riempirla con il risultato??
   let risultatoFinale = ""
 
-     if ( numeroTotale === 'pari' ){
-       
-          risultatoFinale.innerHTML = 'Hai' + ' Vinto ' 
+
+  //creazione prima condizione se il numero tattale creato è uguale ad un nnumero pari
+     if ( numeroTotale === numPari ){
+             
+            if ( sceltanumeri === 'pari' ){
+                risultatoFinale.innerHTML = 'Hai' + ' Vinto ' 
+            }
+            elae if (sceltanumeri === 'dispari'){
+
+                risultatoFinale.innerHTML = 'Hai' + ' Perso ' 
+            }
+         
          }
 
     else if (numeroTotale != numPari){
         
-        risultatoFinale.innerHTML = 'Hai' + ' Perso ' 
+        if ( sceltanumeri === 'pari' ){
+            risultatoFinale.innerHTML = 'Hai' + ' Vinto ' 
+        }
+        elae if (sceltanumeri === 'dispari'){
 
-         }
+            risultatoFinale.innerHTML = 'Hai' + ' Perso ' 
+        }
 
     else {
 
